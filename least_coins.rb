315ce670635ada36d40c85217058fb6c099559fb
AleshaRP <createlele@yahoo.com>
2018-06-21 16:03:26 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,13 @@
 #write out your code here
 
-def least_coins(cents)
+def least_coins(coins)
+cents = {:pennies => 0, :nickels => 0, :dimes => 0, :quarters => 0}
+coins{}
+ while cents %4 == 0
+   quarters += 1
+   puts quarters
 
-#Code your answer here!
+ end
+
 
 end
